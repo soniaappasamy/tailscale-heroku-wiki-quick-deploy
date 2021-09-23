d3a5a8c72a5fc7b320b7e6ae2c1b194258b9f930
Sonia Appasamy <sonia@tailscale.com>
2021-09-23 16:13:30 -0400

    add comments to main.go

--- main.go
+++ main.go
@@ -4,26 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
-var (
-	name       = flag.String("name", "visitor", "The name to say hello to.")
-	publicAddr = flag.String("public", ":80", "The address to bind to.")
-)
-
 func main() {
-	flag.Parse()
-
 	if err := startTailscale(context.Background()); err != nil {
 		log.Fatal(err)
 	}
@@ -40,7 +31,7 @@ const (
 	socketPath    = "/wiki/ts.sock"
 )
 
-const tableQuery = `
+const tsTableQuery = `
 CREATE TABLE IF NOT EXISTS "tailscale_data" (
 	"id"    serial primary key,
 	"state" text not null
@@ -48,34 +39,41 @@ CREATE TABLE IF NOT EXISTS "tailscale_data" (
 `
 
 func startTailscale(ctx context.Context) error {
+	// The TAILSCALE_AUTHKEY is a required environment variable.
+	// Users will see a textfield to add their key on the Heroku deploy dashboard.
+	// When quick-deploying from the admin panel, the key value gets set automatically.
+	tsAuthKey := os.Getenv("TAILSCALE_AUTHKEY")
+
+	// We ask heroku to startup a "heroku-postgresql" add-on (see app.json).
+	// We open up the db here to help us manage our tailscale state file.
 	databaseURL := os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
 		return fmt.Errorf("DATABASE_URL is not set")
 	}
-
-	tsAuthKey := os.Getenv("TAILSCALE_AUTHKEY")
-	if tsAuthKey == "" {
-		return fmt.Errorf("TAILSCALE_AUTHKEY is not set")
-	}
-
 	db, err := sql.Open("postgres", databaseURL+"?sslmode=require")
 	if err != nil {
 		return fmt.Errorf("failed to open database: %w", err)
 	}
 	defer db.Close()
 
-	_, err = db.ExecContext(ctx, tableQuery)
+	// When the Heroku dyno restarts, all state/local storage gets wiped.
+	// To avoid creating new tailscale nodes on each restart (restarts happen fairly frequently on the Heroku free tier),
+	// we store the tailscale state file contents in our postgres db in a "tailscale_data" table.
+	_, err = db.ExecContext(ctx, tsTableQuery)
 	if err != nil {
 		return fmt.Errorf("failed to create tailscale_data table: %w", err)
 	}
 
 	var tsState string
+	// Try to grab our state file contents from the db.
+	// It's fine if nothing is found. That means we likely haven't authenticated tailscale for the first time yet.
 	err = db.QueryRowContext(ctx, "SELECT state FROM tailscale_data ORDER BY id DESC LIMIT 1").Scan(&tsState)
-	if errors.Is(err, sql.ErrNoRows) {
-	} else if err != nil {
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return fmt.Errorf("failed to read state file from database: %w", err)
 	}
 	if tsState != "" {
+		// If we do have state already, write it out to our locally state file.
+		// We will pass this filepath as a flag when starting up tailscale below.
 		err := os.WriteFile(stateFilePath, []byte(tsState), 0644)
 		if err != nil {
 			return fmt.Errorf("failed to write state file: %w", err)
@@ -85,14 +83,14 @@ func startTailscale(ctx context.Context) error {
 		return fmt.Errorf("TAILSCALE_AUTHKEY or state file must be present")
 	}
 
-	// Start `tailscaled`
+	// Start `tailscaled`.
 	daemoncmd := exec.CommandContext(ctx, "/app/tailscaled", "--socket", socketPath, "--state", stateFilePath, "--tun", "userspace-networking")
 	if err = daemoncmd.Start(); err != nil {
 		return fmt.Errorf("failed to start tailscaled: %w", err)
 	}
-	time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second) // TODO: this is hacky
 
-	// Start `tailscale`
+	// Start `tailscale`.
 	args := []string{"--socket", socketPath, "up", "--hostname", "wiki-server"}
 	if tsAuthKey != "" {
 		args = append(args, "--authkey", tsAuthKey)
@@ -102,6 +100,8 @@ func startTailscale(ctx context.Context) error {
 		return fmt.Errorf("failed to start tailscale: %w", err)
 	}
 
+	// Now that we've started up tailscale, store the state file contents back
+	// to the db so we can restore them the next time the dyno restarts.
 	b, err := os.ReadFile(stateFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to read state file: %w", err)
@@ -118,8 +118,8 @@ func startTailscale(ctx context.Context) error {
 
 func startWikiServer(ctx context.Context) error {
 	cmd := exec.CommandContext(ctx, "node", "/wiki/server")
-	// Without specifying the port here, wiki.js use a random port Heroku gives it.
-	// We force it to port 3000 so our go server knows where the service is running.
+	// Without specifying the port here, wiki.js will use the default random public port of the Heroku dyno.
+	// We force it to port 3000 so we can make it only accessible via tailscale.
 	cmd.Env = append(os.Environ(), "PORT=3000")
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("failed to start wiki server: %w", err)
@@ -128,13 +128,14 @@ func startWikiServer(ctx context.Context) error {
 }
 
 // startPublicDummyServer starts a go webserver that displays a simple welcome prompt to viewers.
-// Heroku requires something to be running at it's public port, otherwise it shuts down the instance.
-// We only want our wiki server acessible over Tailscale, though so we don't want to serve that over
-// the public Heroku port. So we instead use this dummy server to satisfy Heroku.
+// Heroku requires something to be running at it's public port, otherwise it shuts down the dyno.
+// We only want our wiki server acessible over tailscale, though so we don't want to serve that over
+// the public Heroku port. Instead, we place this dummy server at the public endpoint.
 func startPublicDummyServer() error {
-	// Grab the Heroku random public port assigned to our instance.
-	if port := os.Getenv("PORT"); port != "" {
-		*publicAddr = ":" + port
+	// Grab the Heroku random public port assigned to our dyno.
+	port := os.Getenv("PORT")
+	if port == "" {
+		return fmt.Errorf("failed to find Heroku public port")
 	}
 
 	hostname, err := os.Hostname()
@@ -144,17 +145,9 @@ func startPublicDummyServer() error {
 
 	publicMux := http.NewServeMux()
 	publicMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome, %s! Hello from %s", *name, hostname)
+		fmt.Fprintf(w, "Welcome! Hello from %s", hostname) // TODO: make this view a little more useful (maybe add instructions for accessing the wiki)
 	})
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		http.ListenAndServe(*publicAddr, publicMux)
-		wg.Done()
-	}()
-
-	wg.Wait()
+	http.ListenAndServe(":"+port, publicMux)
 
 	return nil
 }
